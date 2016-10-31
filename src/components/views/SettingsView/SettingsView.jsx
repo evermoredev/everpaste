621cfb935083c0eef2972a8d28313ed89f7e199f
Nate Brady <nbrady@techempower.com>
2016-10-31 13:44:12 -0700

    adding themes to settings

--- src/components/views/SettingsView/SettingsView.jsx
+++ src/components/views/SettingsView/SettingsView.jsx
@@ -3,7 +3,7 @@ import { observer } from 'mobx-react';
 
 import { HeaderLayout } from 'components/layouts';
 
-@observer(['GlobalStore'])
+@observer(['GlobalStore', 'StyleStore'])
 class SettingsView extends React.Component {
 
   constructor(props) {
@@ -12,11 +12,32 @@ class SettingsView extends React.Component {
     this.props.GlobalStore.currentView = 'SettingsView';
   }
 
+  handleThemeChange = (event, className) => {
+    this.props.StyleStore.setTheme(className);
+  };
+
+  renderThemesList = () => this.props.StyleStore.themes.map((t, idx) =>
+    <div
+      key={idx}
+      className={t.className}
+      onClick={() => this.handleThemeChange(event, t.className)}>
+        <span className="hljs-keyword">A</span>
+        <span className="hljs-type">B</span>
+        <span className="hljs-symbol">C</span>
+        <span className="hljs-string">D</span>
+        {t.name}
+    </div>
+  );
+
   render() {
     return(
       <div className="settings-view">
         <HeaderLayout />
-        Settings View
+        <div className="code-sample">
+        </div>
+        <div className="style-reset">
+          {this.renderThemesList()}
+        </div>
       </div>
     );
   }
--- src/stores/StyleStore/StyleStore.js
+++ src/stores/StyleStore/StyleStore.js
@@ -6,6 +6,47 @@ class StyleStore {
   constructor() {
     this.cookies = cookie.parse(document.cookie);
     this.defaultTheme = this.cookies.theme || 'atom-one-dark-theme';
+
+    this.themes = [
+      { name: 'Agate', className: 'agate-theme' },
+      { name: 'Android Studio', className: 'androidstudio-theme' },
+      { name: 'Arduino Light', className: 'arduino-light-theme' },
+      { name: 'Arta', className: 'arta-theme' },
+      { name: 'Ascetic', className: 'ascetic-theme' },
+      { name: 'Atelier Cave Dark', className: 'atelier-cave-dark-theme' },
+      { name: 'Atelier Cave Light', className: 'atelier-cave-light-theme' },
+      { name: 'Atelier Dune Dark', className: 'atelier-dune-dark-theme' },
+      { name: 'Atelier Dune Light', className: 'atelier-dune-light-theme' },
+      { name: 'Atelier Estuary Dark', className: 'atelier-estuary-dark-theme' },
+      { name: 'Atelier Estuary Light', className: 'atelier-estuary-light-theme' },
+      { name: 'Atelier Forest Dark', className: 'atelier-forest-dark-theme' },
+      { name: 'Atelier Forest Light', className: 'atelier-forest-light-theme' },
+      { name: 'Atelier Heath Dark', className: 'atelier-heath-dark-theme' },
+      { name: 'Atelier Heath Light', className: 'atelier-heath-light-theme' },
+      { name: 'Atelier Lakeside Dark', className: 'atelier-lakeside-dark-theme' },
+      { name: 'Atelier Lakeside Light', className: 'atelier-lakeside-light-theme' },
+      { name: 'Atelier Plateau Dark', className: 'atelier-plateau-dark-theme' },
+      { name: 'Atelier Plateau Light', className: 'atelier-plateau-light-theme' },
+      { name: 'Atelier Savanna Dark', className: 'atelier-savanna-dark-theme' },
+      { name: 'Atelier Savanna Light', className: 'atelier-savanna-light-theme' },
+      { name: 'Atelier Seaside Dark', className: 'atelier-seaside-dark-theme' },
+      { name: 'Atelier Seaside Light', className: 'atelier-seaside-light-theme' },
+      { name: 'Atelier Sulphurpool Dark', className: 'atelier-sulphurpool-dark-theme' },
+      { name: 'Atelier Sulphurpool Light', className: 'atelier-sulphurpool-light-theme' },
+      { name: 'Atom One Dark', className: 'atom-one-dark-theme' },
+      { name: 'Atom One Light', className: 'atom-one-light-theme' },
+      { name: 'Brown Paper', className: 'brown-paper-theme' },
+      { name: 'Codepen Embed', className: 'codepen-embed-theme' },
+      { name: 'Color Brewer', className: 'color-brewer-theme' },
+      { name: 'Darcula', className: 'darcula-theme' },
+      { name: 'Dark', className: 'dark-theme' },
+      { name: 'Docco', className: 'docco-theme' },
+      { name: 'Dracula', className: 'dracula-theme' },
+      { name: 'Far', className: 'far-theme' },
+      { name: 'Foundation', className: 'foundation-theme' },
+      { name: 'Github', className: 'github-theme' },
+      { name: 'Github Gist', className: 'github-gist-theme' }
+    ];
   }
 
   @observable theme = this.defaultTheme;
